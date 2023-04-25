7a24e21c60c0db8f49c5386b7610e97c6d7f2f8b
Manoel-I <manoel.122112@outlook.com>
2023-04-24 23:16:54 -0300

    Game over animation and sound

--- js/script.js
+++ js/script.js
@@ -14,12 +14,33 @@ const jump = () => {
 
 // definindo o hitbox para game-over
 const loop = setInterval(()=>{
+    // pegando o valor posição do cano
     const pipePosition = pipe.offsetLeft
     console.log(pipePosition);
 
-    if(pipePosition <= 120){
+    //pegando a posição de pulo do mario
+    //          #colocando o "+" na frente da string tranforma em numero
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');//posição computada no estilo da imagem
+    console.log(marioPosition);
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = 'none';
         pipe.style.left = pipePosition+'px';
+
+        mario.style.animation = 'none';
+        mario.style.bottom = marioPosition+'px';
+
+        //trocando o mario normal pelo mario quando dá game over
+        mario.src = "./imagens/mario-gameover.png";
+        mario.style.width = '70px'
+        mario.style.marginLeft = '50px';
+
+        const gameoverSound = document.querySelector('audio');
+        gameoverSound.play().then(setTimeout(()=>{
+            gameoverSound.pause();
+            return;
+        }, 7000));
+    
     } 
 
 }, 10);
